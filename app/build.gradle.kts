da846b1b9d1c83934e5a04f7223a78739a59c765
unknown <khu0mmh@gmail.com>
2024-10-14 10:56:40 +0700

    agp change, ksp migration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -48,5 +49,7 @@ dependencies {
 
     implementation("de.hdodenhof:circleimageview:3.1.0") //buat lingkaran
 
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 
 }
