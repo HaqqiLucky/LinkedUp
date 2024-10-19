e565a8ec156ea06278d0d619327c1c5f95e1251b
HaqqiLucky <haqqiarifin@outlook.com>
2024-10-19 19:38:04 +0700

    nambahin email sama password, sisanya di buat nullable buat registerlogin

--- app/src/main/java/com/example/linkedup/utils/User.kt
+++ app/src/main/java/com/example/linkedup/utils/User.kt
@@ -8,14 +8,16 @@ import androidx.room.PrimaryKey
 data class User(
     @PrimaryKey(autoGenerate = true) val _id: Int = 0,
     val name: String,
-    val alamat: String,
+    val alamat: String? = null,
+    val email: String,
+    val password: String,
 //    @ColumnInfo (name = "no_tlpn") val telepon : String,
 //    val pengalaman: String,
-    val deskripsi : String,
-    @ColumnInfo (name = "gender") val jenis_kelamin : String,
+    val deskripsi : String? = null,
+    @ColumnInfo (name = "gender") val jenis_kelamin : String? = null,
 //    @ColumnInfo (name = "edukasi") val riwayat_edukasi: String,
 //    @ColumnInfo (name = "domisili") val alamat_saat_ini: String,
 //    @ColumnInfo (name = "isAdmin") val isAdmin : Boolean,
-    val image: String
+    val image: String? = null
 
 )
