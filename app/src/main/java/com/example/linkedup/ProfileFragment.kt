cf1512025351030cc423c3e9fa0c2715578bf9a6
HaqqiLucky <haqqiarifin@outlook.com>
2024-10-20 15:12:42 +0700

    update nama di register sudah bisa masuk ke profil

--- app/src/main/java/com/example/linkedup/ProfileFragment.kt
+++ app/src/main/java/com/example/linkedup/ProfileFragment.kt
@@ -1,17 +1,33 @@
 package com.example.linkedup
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.linkedup.Profile.EditProfileFragment
-import com.example.linkedup.databinding.FragmentPostBinding
 import com.example.linkedup.databinding.FragmentProfileBinding
+import com.example.linkedup.item.LokerViewModel
+import com.example.linkedup.item.SessionViewModel
+import com.example.linkedup.utils.User
+import kotlinx.coroutines.launch
 
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var sessionViewModel: SessionViewModel
+    private lateinit var user: User
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sessionViewModel = ViewModelProvider(requireActivity()).get(SessionViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,12 +35,27 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        user = loadUserFromPreferences() ?: User(-1, "Guest", null, "", "", null, null, false, null)
+        binding.namauser.text = user.name
+//        binding.desk. = user.deskripsi
+//        binding.alamat.text =
+
+
+        sessionViewModel = ViewModelProvider(this).get(SessionViewModel::class.java)
         binding.lihatdetailexperience.setOnClickListener {
             val experienceFragment = LihatDetailPengalamanFragment()
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, experienceFragment)
                 .addToBackStack(null)
                 .commit()
+        }
+        lifecycleScope.launch {
+            try {
+                Log.d("lalala", "userId value: ${sessionViewModel.userId.value}")
+                sessionViewModel.userId.value?.let { Log.d("lalala", it.toString()) }
+            } catch (e: Exception) {
+                Log.e("Lalalala", "Error inserting data", e)
+            }
         }
 
         binding.tombolEditTabelUser.setOnClickListener{
@@ -37,6 +68,25 @@ class ProfileFragment : Fragment() {
 
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Inisialisasi ViewModel
+
+    }
+    private fun loadUserFromPreferences(): User? {
+        val sharedPref = requireActivity().getSharedPreferences("user_prefs", MODE_PRIVATE)
+        val userId = sharedPref.getInt("user_id", -1)
+        val userName = sharedPref.getString("user_name", null) ?: return null
+        val userAlamat = sharedPref.getString("user_alamat", null)
+        val userEmail = sharedPref.getString("user_email", null) ?: return null
+        val userPassword = sharedPref.getString("user_password", null) ?: return null // Hati-hati, ini bisa berisiko
+        val userDeskripsi = sharedPref.getString("user_deskripsi", null)
+        val userGender = sharedPref.getString("user_gender", null)
+        val userIsAdmin = sharedPref.getBoolean("user_isAdmin", false)
+        val userImage = sharedPref.getString("user_image", null)
+
+        return User(userId, userName, userAlamat, userEmail, userPassword, userDeskripsi, userGender, userIsAdmin, userImage)
+    }
 
 }
