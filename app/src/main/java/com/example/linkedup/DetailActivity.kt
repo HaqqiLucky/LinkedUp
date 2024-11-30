759e2da3bd99d9fd3160bc189b3d108de387bc09
unknown <khu0mmh@gmail.com>
2024-11-30 11:21:50 +0700

    menambahkan fungsi lihat detail

--- app/src/main/java/com/example/linkedup/DetailActivity.kt
+++ app/src/main/java/com/example/linkedup/DetailActivity.kt
@@ -6,23 +6,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.linkedup.databinding.ActivityDetailBinding
+import com.example.linkedup.fetch.ConfigManager
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val titleView: TextView = findViewById(R.id.job_title)
-        val gajiView: TextView = findViewById(R.id.job_salary)
-        val deskripsiView: TextView = findViewById(R.id.job_description)
-        val companyView: TextView = findViewById(R.id.company_name)
-        val waktuView: TextView = findViewById(R.id.posting_date)
 
         val intent = intent
         val title = intent.getStringExtra("title")
@@ -30,12 +29,20 @@ class DetailActivity : AppCompatActivity() {
         val deskripsi = intent.getStringExtra("deskripsi")
         val waktu = intent.getStringExtra("waktu")
         val company = intent.getStringExtra("company")
+        val image = intent.getStringExtra("image")
 
-        titleView.text = title.toString()
-        gajiView.text = "Salary : Rp."+gaji.toString()
-        deskripsiView.text = deskripsi.toString()
-        companyView.text = "Company : "+company.toString()
-        waktuView.text = "Posted on : "+waktu.toString()
+        val BASE_URL = ConfigManager.getBaseUrl()
+        val imageUrl = "${BASE_URL}${image}"
+        Glide.with(this)
+            .load(imageUrl)
+            .error(R.drawable.headtest)
+            .into(binding.jobImage)
+
+        binding.jobTitle.text = title.toString()
+        binding.jobSalary.text = "Salary : Rp."+gaji.toString()
+        binding.jobDescription.text = deskripsi.toString()
+        binding.companyName.text = "Company : "+company.toString()
+        binding.postingDate.text = "Posted on : "+waktu.toString()
 
 
     }
